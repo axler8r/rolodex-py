f4303c9c5ba9868b4df32543b0c804e21adc40b2
axler8tor <axl.mattheus@gmail.com>
2020-03-21 08:05:29 +1300

    Implement create pattern

--- rolodex/address.py
+++ rolodex/address.py
@@ -1,6 +1,8 @@
 from enum import IntEnum
 from dataclasses import dataclass
 
+from rolodex import logger
+
 
 class AddressKind(IntEnum):
     ''' Kind of telepohone number '''
@@ -12,12 +14,53 @@ class AddressKind(IntEnum):
 
 @dataclass(order=True, frozen=True)
 class Address:
-    unit_number: str = None
-    unint: str = None
+    unit_number: str
+    unit: str
     street_number: str
     street: str
-    suburb: str = None
+    suburb: str
     city: str
     code: str
-    country: str = None
+    country: str
     kind: AddressKind = AddressKind.Unspecified
+
+
+def create(
+        street_number: str = None,
+        street: str = None,
+        city: str = None,
+        code: str = None,
+        kind: AddressKind = AddressKind.Unspecified,
+        **kwargs
+) -> Address:
+    unit_number: str = kwargs.get('unit_number', None)
+    unit: str = kwargs.get('unit', None)
+    suburb: str = kwargs.get('suburb', None)
+    country: str = kwargs.get('country', None)
+
+    logger.debug("unit_number={}", unit_number)
+    logger.debug("unit={}", unit)
+    logger.debug("street_number={}",  street_number)
+    logger.debug("street={}", street)
+    logger.debug("suburb={}", suburb)
+    logger.debug("city={}", city)
+    logger.debug("code={}", code)
+    logger.debug("country={}", country)
+    logger.debug("kind={}", kind)
+
+    if not street_number:
+        raise ValueError("street_number cannot be empty or None")
+    
+    if not street:
+        raise ValueError("steet cannot be empty or None")
+    
+    if not city:
+        raise ValueError("city cannot be empty or None")
+    
+    if not code:
+        raise ValueError("code cannot be empty or None")
+
+    result = Address(unit_number, unit, street_number, street,
+                     suburb, city, code, country, kind)
+    logger.debug("result={}", result)
+    return result
