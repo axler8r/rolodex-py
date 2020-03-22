53a024a1572be366aa828ecd6ed09344da849fc2
axler8tor <axl.mattheus@gmail.com>
2020-03-22 19:18:08 +1300

    Implement telephone numnber

--- rolodex/telephone_number.py
+++ rolodex/telephone_number.py
@@ -1,6 +1,8 @@
 from enum import IntEnum
 from dataclasses import dataclass
 
+from rolodex import logger
+
 
 class TelephoneNumberKind(IntEnum):
     ''' Kind of telepohone number '''
@@ -18,3 +20,30 @@ class TelephoneNumber:
     number: str
     extension: str
     kind: TelephoneNumberKind = TelephoneNumberKind.Unspecified
+
+
+def create(
+        area_code=None,
+        number=None,
+        kind=TelephoneNumberKind.Unspecified,
+        **kwargs
+) -> TelephoneNumber:
+    coutry_code = kwargs.get('country_code', None)
+    extension = kwargs.get('extension', None)
+
+    logger.debug('country_code={}', coutry_code)
+    logger.debug('area_code={}', area_code)
+    logger.debug('number={}', number)
+    logger.debug('extension={}', extension)
+    logger.debug('kine={}', kind)
+
+    if not area_code:
+        raise ValueError('area_code cannot be empty or None')
+
+    if not number:
+        raise ValueError('number cannot be empty or None')
+
+    result: TelephoneNumber = TelephoneNumber(
+        coutry_code, area_code, number, extension, kind)
+    logger.debug('result={}', result)
+    return result
